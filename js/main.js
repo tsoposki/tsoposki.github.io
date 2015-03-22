4cec6c7c1d7a9f163910abc9c5006c85fc3da2ce
Trajche Shoposki <tsoposki@gmail.com>
2015-03-22 15:08:29 +0100

    optimized

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 $(function() {
-    $controller = new ScrollMagic;
-    
-    (new ScrollScene({
-        triggerElement: "#main .mainPicture",
-        triggerHook: 0,
-        duration: $(window).height()
-    })).addTo($controller).setTween(TweenMax.fromTo("#main .mainPicture", 1, {
-        y: 0,
-        z: 0.01
-    }, {
-        y: 250,
-        z: 0.01,
-        ease: Linear.easeNone
-    }));
+    // init controller
+    var controller = new ScrollMagic.Controller();
+
+    var target = $("#main .mainPicture");
+    var targetHeight = target.css("height");
+
+    var steppedEase = new SteppedEase(7);
+
+    var tween = TweenMax.fromTo(
+            target, 0.5, {
+                y: 0,
+                z: 0.01, 
+                force3D: "true", 
+                ease: steppedEase
+            },
+            {
+                y: 280,
+                z: 0.01, 
+                force3D: "true",
+                ease: steppedEase
+            }
+
+    );
+
+    // build scene
+    var scene = new ScrollMagic.Scene({triggerElement: "nothing", duration: targetHeight})
+                    .setTween(tween)
+                    .addTo(controller);
+
 
 });
+
+
