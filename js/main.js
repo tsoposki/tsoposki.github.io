6b0abfe51709d8f97ebd9a18dad0bd636e92c9b7
Trajche Shoposki <tsoposki@gmail.com>
2015-03-22 01:01:01 +0100

    deleted scroll handler

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 $(function() {
     $controller = new ScrollMagic;
     
-    trajche();
-    
     (new ScrollScene({
         triggerElement: "#main .mainPicture",
         triggerHook: 0,
         duration: $(window).height()
-    })).addTo($controller).setTween(TweenMax.fromTo("#main .mainPicture", 0.7, {
+    })).addTo($controller).setTween(TweenMax.fromTo("#main .mainPicture", 1, {
         y: 0,
         z: 0.01
     }, {
@@ -17,20 +15,3 @@ $(function() {
     }));
 
 });
-
-function trajche() {
-    var a = $(window);
-    a.on("mousewheel DOMMouseScroll", function(b) {
-        b.preventDefault();
-        b = b.originalEvent.wheelDelta / 120 || -b.originalEvent.detail / 3;
-        b = (a.scrollTop() - parseInt(280 * b)).toFixed(2);
-        TweenMax.to(a, 0.7, {
-            scrollTo: {
-                y: b,
-                z: 0.01,
-                autoKill : true // Allow scroll position to change outside itself
-            },
-            ease: Linear.easeNone
-        })
-    })
-}
